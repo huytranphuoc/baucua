d3be3aab3584f2b4050a875c02bc7cfdcc0c526d
huytranphuoc <51155932+huytranphuoc@users.noreply.github.com>
2021-12-06 13:53:29 +0700

    tests

--- src/index.js
+++ src/index.js
@@ -56,8 +56,7 @@ app.post('/notification', async(req, res) => {
 });
 
 
-var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
+app.listen(process.env.PORT || 3000);
 
 // app.listen(process.env.PORT || 3000, function(){
 //     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
