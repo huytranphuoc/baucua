59a407d199f4ed097dd7a37c8a79a1e183d664ee
huytranphuoc <51155932+huytranphuoc@users.noreply.github.com>
2021-12-06 13:29:14 +0700

    tests

--- src/index.js
+++ src/index.js
@@ -59,10 +59,10 @@ app.post('/notification', async(req, res) => {
 const ip = "127.0.0.1"; // process.env.IP || "127.0.0.1";
 const port = 3002; // process.env.PORT || 3002;
 
-// server.listen(port, ip, function() {
-//     console.log("Express server listening at %s:%d ", ip, port);
-// });
+server.listen(port, ip, function() {
+    console.log("Express server listening at %s:%d ", ip, port);
+});
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+// app.listen(process.env.PORT || 3000, function(){
+//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+//   });
