7c32bd06c8b699a4243796ad4ac98796ed3dfab2
huytranphuoc <51155932+huytranphuoc@users.noreply.github.com>
2021-12-07 23:31:19 +0700

    tests

--- src/index.js
+++ src/index.js
@@ -67,14 +67,14 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const app = express();
+const app = express().listen(PORT, () => console.log(`Listening on ${ PORT }`));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-app
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+// app
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
   app.get('/', (req, res) => {
     res.send("Home page. Server running okay.");
 });
