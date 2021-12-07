108c7840a050e4b3d6891da73c5103c51e3a5761
huytranphuoc <51155932+huytranphuoc@users.noreply.github.com>
2021-12-07 23:24:42 +0700

    tests

--- src/index.js
+++ src/index.js
@@ -64,9 +64,7 @@ app.get('/', (req, res) => {
 // app.listen(process.env.PORT || 3000, function(){
 //     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 //   });
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+
 
 app
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
